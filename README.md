2e0ea44b2ca76093a3cc94bbd1baef9b7f6bcd0f
Piotr Dulian <piotrdulian53@gmail.com>
2025-04-30 15:32:58 +0200

    Version 0.2.0

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# qmetro
+# <span style="color:red">Q</span>Metro++
+## Python package for large scale quantum metrology
+This is version beta.   
+More info: p.dulian@cent.uw.edu.pl
---
+++ qmetro/__init__.py
@@ -0,0 +1,8 @@
+from .bounds import (
+    par_bound_single_n, par_bounds, ad_bounds, cs_bounds, par_ad_cs_bounds,
+    asym_scaling_qfi, ad_bounds_correlated, ad_asym_bound_correlated
+)
+from .iss_opt import iss_opt
+from .param_channel import *
+from .protocols import *
+from .qmtensor import *
