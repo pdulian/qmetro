91f52a12aa4647d9ebb00cdcd712dcc5fd4238bb
Piotr Dulian <piotrdulian53@gmail.com>
2025-06-20 10:20:28 +0200

    Configure Read the Docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_baseurl = '/qmetro/'
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['/qmetro/_static']
+html_static_path = ['/_static']
--- docs/sphinx/install.rst
+++
@@ -1,3 +0,0 @@
-Installation guide
-==============
-TODO
