74ad2cfa5e412eeabe69826b4f3b89a0fc2e7f59
Piotr Dulian <piotrdulian53@gmail.com>
2026-01-14 14:55:45 +0100

    Update requirements.txt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'QMetro++'
 copyright = '2025, Piotr Dulian'
 author = 'Piotr Dulian'
-release = '1.0.0'
+release = '1.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
